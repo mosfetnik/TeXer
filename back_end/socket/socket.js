8eee471ace8d7d1a91f2e422b7c4134065018dfc
mosfetNik <adityarai424@gmail.com>
2024-03-16 13:17:37 +0530

    Socket Url updated again

--- back_end/socket/socket.js
+++ back_end/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://texer.onrender.com/"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
