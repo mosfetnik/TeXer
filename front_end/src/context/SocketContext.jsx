3c6d125041959fbf6dc9257eb4678fded3ec89cf
mosfetNik <adityarai424@gmail.com>
2024-03-16 13:28:12 +0530

    updated again

--- front_end/src/context/SocketContext.jsx
+++ front_end/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://texer.onrender.com/", {
+      const socket = io("https://texer.onrender.com", {
         query: {
           userId: authUser._id,
         },
